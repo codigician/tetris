ea6c0796093ca0d03803e5f02ec184112db78abb
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-10-31 17:26:38 +0300

    Refactor move functions

--- src/tetris.py
+++ src/tetris.py
@@ -3,7 +3,10 @@ from grid import TetrisGrid
 from grid import TetrisVirtualGrid
 from shape import Shape
 from shape import create_shape
+
 import random
+import typing
+
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -17,7 +20,7 @@ class Tetris:
         self.virtual_grid = TetrisVirtualGrid(10, 10, self.grid.sync)
         self.shape_generator = RandomShapeGenerator(0, 4)
 
-        self.active_shape: Shape = self.shape_generator.generate()
+        self.active_shape: Shape = None
         self.held_shape: Shape = None
         self.is_shape_exchanged = False
 
@@ -29,7 +32,8 @@ class Tetris:
         Initialize the grid and the active shape
         Start gravity thread to move the active shape down
         """
-        pass
+        self.active_shape: Shape = self.shape_generator.generate()
+        self.virtual_grid.add_shape(self.active_shape)
 
     def gameover(self) -> None:
         """gameover ends the game, output the score and level"""
@@ -37,32 +41,20 @@ class Tetris:
 
     def move_left(self):
         """move the active shape left direction"""
-        try:
-            self.virtual_grid.relocate_shape(self.active_shape, 0, -1)
-        except OccupiedPositionException:
-            pass
+        self.__move(col=-1)
 
     def move_right(self):
         """move the active shape right direction"""
-        try:
-            self.virtual_grid.relocate_shape(self.active_shape, 0, 1)
-        except OccupiedPositionException:
-            pass
+        self.__move(col=1)
 
     def move_down(self):
         """move the active shape down direction"""
-        try:
-            self.virtual_grid.relocate_shape(self.active_shape, 1, 0)
-        except OccupiedPositionException:
-            self.__add_new_shape()
+        self.__move(row=1, onfail=self.__add_new_shape)
 
     def move_ground(self):
         """move the active shape to the ground"""
-        try:
-            while True:
-                self.virtual_grid.relocate_shape(self.active_shape, 1, 0)
-        except OccupiedPositionException:
-            self.__add_new_shape()
+        while self.__move(row=1, onfail=self.__add_new_shape):
+            pass
 
     def hold(self) -> None:
         """hold the active shape, If there is already a held shape, swap them"""
@@ -82,6 +74,14 @@ class Tetris:
             self.is_shape_exchanged = True
         except OccupiedPositionException:
             pass
+
+    def __move(self, row=0, col=0, onfail: typing.Callable = None) -> bool:
+        try:
+            self.virtual_grid.relocate_shape(self.active_shape, row, col)
+            return True
+        except OccupiedPositionException:
+            onfail()
+            return False
 
     def __add_new_shape(self):
         self.active_shape = self.shape_generator.generate()
@@ -103,5 +103,35 @@ class RandomShapeGenerator:
         return create_shape(shape_type, self.row, self.col, shape_color)
 
 
+def render(m, title=None):
+    print()
+    print(title)
+    for row in m:
+        for col in row:
+            if col is None:
+                print('-', end=' ')
+            else:
+                print('U', end=' ')
+        print()
+
+
 if __name__ == "__main__":
-    print('hello world')
+    tetris = Tetris()
+    tetris.play()
+    render(tetris.grid.get_map())
+    tetris.move_down()
+    render(tetris.grid.get_map())
+    tetris.move_ground()
+    render(tetris.grid.get_map())
+    tetris.move_right()
+    render(tetris.grid.get_map())
+    tetris.hold()
+    render(tetris.grid.get_map(), "HOLD")
+    tetris.hold()
+    render(tetris.grid.get_map(), "HOLD")
+    tetris.hold()
+    render(tetris.grid.get_map(), "HOLD")
+    tetris.move_ground()
+    render(tetris.grid.get_map(), "GROUND")
+    tetris.hold()
+    render(tetris.grid.get_map(), "HOLD")
